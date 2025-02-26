c5f8244894624619ab90154b50f342e8160cecdd
sattwikmajumder <majumdersattwik@gmail.com>
2025-02-26 17:19:31 +0000

    Wed, Feb 26, 2025, 9:19 AM -08:00

--- website1/loading.js
+++ website1/loading.js
@@ -1,4 +1,9 @@
 const loadText = document.querySelector('.loading-text')
 const bg = doucment.querySelector
-let load = 0 
+let load = 0
 loadText.innerText = "80&"
+function blurring()
+{
+    
+}
+loadText.innerText = `${load}%`
