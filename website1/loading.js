65a05952a1af23bb0a98d75978b1f1d8c0d595c7
sattwikmajumder <majumdersattwik@gmail.com>
2025-02-26 09:26:04 -0800

    something

--- website1/loading.js
+++ website1/loading.js
@@ -2,8 +2,14 @@ const loadText = document.querySelector('.loading-text')
 const bg = doucment.querySelector
 let load = 0
 loadText.innerText = "80&"
+let int = setInterval(blurring,300)
 function blurring()
 {
-    
+    load = load++
+    if (load > 99)
+        {
+            clearInterval(int)
+        }
+    loadText.innerText = `${load}%`
 }
-loadText.innerText = `${load}%`
+
