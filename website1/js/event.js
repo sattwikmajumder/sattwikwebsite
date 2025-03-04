3e50f2b7f637329b97b5b58190be5713c74e5784
sattwikmajumder <majumdersattwik@gmail.com>
2025-03-04 09:56:28 -0800

    something

---
+++ website1/js/event.js
@@ -0,0 +1,10 @@
+doucment.addEventListener('keyup',(event)=>
+    {
+        console.log(event.key)
+
+        if(event.key == 'ArrowDown')
+            {
+                positionY = positionY + 10
+            }
+            element3
+    })
