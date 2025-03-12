8f7e567a84d6c5f14fa781ad7e3048db5a6cf5e7
sattwikmajumder <majumdersattwik@gmail.com>
2025-03-12 09:51:51 -0700

    Initial commit

--- sound.js
+++ sound.js
@@ -1,1 +1,12 @@
-co
+const sounds = ['applause,' 'wrong']
+
+sounds.forEach(sound => {
+    const btn = doucment.createElement('button')
+    btn.classList.add('btn')
+    btn.innerHTML = sound
+    btn.addEventListener('click',()=>{
+        doucment.getElementById(sound).play()
+    })
+
+    doucment.getElementById('buttons').appendChild(btn)
+})
