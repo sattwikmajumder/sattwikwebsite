567546eca69ced8f49e45df0a1f71e1948d459b6
sattwikmajumder <majumdersattwik@gmail.com>
2025-04-02 08:55:14 -0700

    Initial commit

---
+++ relaxer.js
@@ -0,0 +1,11 @@
+const container = document.getElementById('container')
+const text = document.getElementById('text')
+
+function breathAnimation()
+{
+    text.innerHTML = 'Breathe in'
+    container.classList.add('grow');
+
+
+    setTimeout(?,3000)
+}
