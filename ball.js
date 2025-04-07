4df6547141fa45e0afa3e440c634c4742d4caee3
sattwikmajumder <majumdersattwik@gmail.com>
2025-04-07 16:56:01 +0000

    Mon, Apr 7, 2025, 9:56 AM -07:00

--- ball.js
+++ ball.js
@@ -1,23 +1,34 @@
 const ball = document.createElement('div')
+document.body.appendChild(ball)
+const LPadel = document.creteElement('div')
+document.body.appendChild(LPadel)
+let LPadelWidth = 20
+let lPadelHeight = 100
 const ballRadius = 30
 const windowHeight = window.innerHeight
 const windowWidth = window.innerWidth
-let ballXPosition = windowWidth / 2 - ballRadius
+let ballXPosition = windowWidth/2 - ballRadius
+let ballYPosition = windowHeight/2 - ballRadius
 let BallSpeed = 5
 let ballXDirection = 1
-let ballYDirection = 1
+
 setInterval(moveBall(), 10)
 
 function moveBall() {
     ball.style.left = ballXPosition + BallSpeed * ballXDirection
     ball.style.left = `${ballXPosition}px`
-    if (ballposition < 0 || ballXPosition > windowWidth - 2 * ballRadius) {
+    if (ballXPosition > windowWidth ){
         ballXDirection = ballXDirection * -1
     }
+    if (ballXPosition < 0 || ballXPosition > windowWidth - 2 * ballRadius )
+    {
+        ballXDirection = ballXDirection * -1
+    }
+
 }
 createBall()
 
-function createBall() {
+function createBall(){
     document.body.appendChild(ball)
     ball.style.height = `${2 * ballRadius }px`
     ball.style.width = `${2 * ballRadius }px`
@@ -26,4 +37,14 @@ function createBall() {
     ball.style.position = "absolute"
     ball.style.top = `${windowHeight/2 - ballRadius}px`
     ball.style.left = `${windowWidth/2 - ballRadius}px`
+}
+
+
+
+create LPadel() {
+    LPadel.style.height = `${LPadelHeight}px`
+    LPadel.style.width =  `${LPadelWidth}px`
+    LPadel.style.backgroundColor = `blue`
+    LPadel.style.position = `absolute`
+    LPadel.style.left = "
 }
