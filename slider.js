4c638f0904d8c3132ca73fb4d62d1333661b1709
sattwikmajumder <majumdersattwik@gmail.com>
2025-05-29 16:43:02 -0700

    Initial commit

--- slider.js
+++ slider.js
@@ -1,27 +1,21 @@
-const upButton = document.querySelector('.up-button');
-const downButton = document.querySelector('.down-button');
-const rightSide = document.querySelector('.right-side');
-const leftSide = document.querySelector('.left-side');
-const slidesLength = rightSide.querySelectorAll('div').length;
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+const rightSide = document.querySelector('.right-side')
+const leftSide = document.querySelector('.left-side')
+const slidesLength = rightSide.querySelectorAll('div').length
 
-// First slide
-let currentSlide = 1; // slidesLength = 4
-leftSide.style.top = `${-(slidesLength - currentSlide) * 100}%`;
 
-upButton.addEventListener('click', () => changeSlide('up'));
-downButton.addEventListener('click', () => changeSlide('down'));
+let currentSlide = 1 //slideLengt =4
+leftSide.style.top = `${-(slidesLength - currentSlide)*100}%`
 
-function changeSlide(direction) {
-    if (direction === 'up') {
-        if (currentSlide === slidesLength) {
-            currentSlide = 0;
-        }
-        currentSlide += 1;
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+function changeSlide(direction){
+    if (direction === 'up'){
+        currentSlide = (currentSlide % slidesLength) + 1;
     } else {
-        currentSlide -= 1;
-        if (currentSlide === 0) {
-            currentSlide = slidesLength;
-        }
+        currentSlide = (currentSlide - 1) || slidesLength;
     }
 
     leftSide.style.top = `${-(slidesLength - currentSlide) * 100}%`;
