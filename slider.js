1914709d96f725c6b518e3da4143b21524fdca24
sattwikmajumder <majumdersattwik@gmail.com>
2025-05-29 17:20:37 +0000

    Thu, May 29, 2025, 10:20 AM -07:00

---
+++ slider.js
@@ -0,0 +1,29 @@
+const upButton = document.querySelector('.up-button');
+const downButton = document.querySelector('.down-button');
+const rightSide = document.querySelector('.right-side');
+const leftSide = document.querySelector('.left-side');
+const slidesLength = rightSide.querySelectorAll('div').length;
+
+// First slide
+let currentSlide = 1; // slidesLength = 4
+leftSide.style.top = `${-(slidesLength - currentSlide) * 100}%`;
+
+upButton.addEventListener('click', () => changeSlide('up'));
+downButton.addEventListener('click', () => changeSlide('down'));
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        currentSlide += 1;
+        if (currentSlide > slidesLength) {
+            currentSlide = 1;
+        }
+    } else {
+        currentSlide -= 1;
+        if (currentSlide < 1) {
+            currentSlide = slidesLength;
+        }
+    }
+
+    leftSide.style.top = `${-(slidesLength - currentSlide) * 100}%`;
+    rightSide.style.top = `${-(currentSlide - 1) * 100}%`;
+}
